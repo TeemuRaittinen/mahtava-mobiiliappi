798e1ecc498fb699caef8342aeadbe2007aaded6
Erja <erjapisila@yahoo.com>
2024-10-03 17:05:02 +0300

    fix babel-config

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,9 @@ module.exports = {
     ['module:react-native-dotenv', {
       moduleName: '@env',
       path: '.env',
-    }]
+    }],
+    ['@babel/plugin-transform-private-methods', { loose: true }], // Ensure loose mode is enabled
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }], // Also add this plugin with loose mode
+    ['@babel/plugin-transform-class-properties', { loose: true }],
   ],
 };
